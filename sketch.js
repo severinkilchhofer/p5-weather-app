c243cf5d37b4431ca7c66264a855e2b84a2b2e39
Severin Kilchhofer <>
2019-01-25 10:28:25 +0100

    code cleaning

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ let textCoordinates;
 let textCoordinates2;
 let iconCoordinates;
 let iconCoordinates2;
+
 let rectangleXSun;
 let rectangleXRain;
 let rectangleXTemp;
@@ -20,8 +21,6 @@ let rectangleXSun2;
 let rectangleXRain2;
 let rectangleXTemp2;
 let rectangleXWind2;
-let rectangleXStart = 0;
-let rectangleXEnd = 0;
 
 let currentWeather = [];
 let currentCities = [];
@@ -62,13 +61,7 @@ function setup() {
 
 function draw() {
     background(30, 30, 30);
-
-
     createRectangle();
-    // createRectangleDown();
-
-    // createCircles();
-    // createDots();
 }
 
 function setCities() {
@@ -98,9 +91,7 @@ function createRectangle() {
         upperRow++;
 
         let cityName = currentWeather.find(weather => weather.location.name === currentCity);
-
         let day = cityName.current.is_day;
-        console.warn('day', day);
 
         uv = cityName.current.uv;
         precip = cityName.current.precip_mm;
@@ -244,229 +235,4 @@ function createRectangle() {
             }
         }
     }
-}
-
-// v2
-function createCircles() {
-    colorMode(RGB, 255, 255, 255, 1);
-
-    let sun;
-
-    for (let currentCity of currentCities) {
-
-        const cityName = currentWeather.find(weather => weather.location.name === currentCity);
-
-        uv = cityName.current.uv;
-        precip = cityName.current.precip_mm;
-        temp = cityName.current.temp_c;
-        wind = cityName.current.wind_kph;
-
-        // sun
-        let sunValueMap = map(uv, 0, 10, 0, 1);
-        let SunColorfrom = color('#fff0a8');
-        let SunColoeto = color('#f8b300');
-        sun = lerpColor(SunColorfrom, SunColoeto, sunValueMap);
-
-
-        // precip
-        let rainValueMap = map(precip, 0, 3, 0, 1);
-        let rainValue = precip;
-        let rainColorfrom = color('#a8d0e6');
-        let rainColoeto = color('#24305e');
-        let rain = lerpColor(rainColorfrom, rainColoeto, rainValueMap);
-
-
-        // temperatur
-        let tempValueMap = map(temp, -35, 40, 0, 1);
-        let tempValue = temp;
-        let tempColorfrom = color('#99fbb8');
-        let tempColoeto = color('#035117');
-        let temperature = lerpColor(tempColorfrom, tempColoeto, tempValueMap);
-
-
-        // wind
-        let windValueMap = map(wind, 0, 80, 0, 1);
-        let windValue = wind;
-        let windColorfrom = color('#F397D6');
-        let windColoeto = color('#231942');
-        let windy = lerpColor(windColorfrom, windColoeto, windValueMap);
-
-        noStroke();
-
-        let strokeSun = sunValueMap * 100;
-        let strokeRain = rainValue * 20;
-        let strokeTemperature = tempValue * 2;
-        let strokeWind = windValue / 10;
-
-        //sun
-        noFill();
-        stroke(sun);
-        strokeWeight(strokeSun);
-        ellipse(rectangleXStart += 300, 350, 100, 100);
-
-
-        // rain
-        stroke(rain);
-        strokeWeight(strokeRain);
-        ellipse(rectangleXStart, 350, 100, 100);
-        // line(rectangleXRain += 300, 350, rectangleXRain, 100);
-        // line(30, 20, 85, 75);
-        // rect(rectangleXRain += 300, 350, 100, 100);
-
-        stroke(temperature);
-        strokeWeight(strokeTemperature);
-        ellipse(rectangleXStart, 350, 100, 100);
-        // rect(rectangleXTemp += 300, 450, 100, 100);
-        // line(rectangleXTemp += 300, 350, rectangleXTemp, rectangleXTemp - 100);
-
-        stroke(windy);
-        strokeWeight(strokeWind);
-        ellipse(rectangleXStart, 350, 100, 100);
-
-        noStroke();
-        fill(0);
-        textSize(20);
-        text(cityName.location.name, textCoordinates += 300, 600);
-
-
-    }
-}
-
-// v3
-function createDots() {
-    colorMode(RGB, 255, 255, 255, 1);
-
-    let sun;
-    let circles = [];
-    let protection = 0;
-
-    for (let currentCity of currentCities) {
-
-        console.log('createDots');
-
-        const cityName = currentWeather.find(weather => weather.location.name === currentCity);
-
-        uv = cityName.current.uv;
-        precip = cityName.current.precip_mm;
-        temp = cityName.current.temp_c;
-        wind = cityName.current.wind_kph;
-
-        // sun
-        let sunValueMap = map(uv, 0, 10, 0, 1);
-        let SunColorfrom = color('#fff0a8');
-        let SunColoeto = color('#f8b300');
-        sun = lerpColor(SunColorfrom, SunColoeto, sunValueMap);
-
-
-        // precip
-        let rainValueMap = map(precip, 0, 3, 0, 1);
-        let rainValue = precip;
-        let rainColorfrom = color('#a8d0e6');
-        let rainColoeto = color('#24305e');
-        let rain = lerpColor(rainColorfrom, rainColoeto, rainValueMap);
-
-
-        // temperatur
-        let tempValueMap = map(temp, -35, 40, 0, 1);
-        let tempValue = temp;
-        let tempColorfrom = color('#99fbb8');
-        let tempColoeto = color('#035117');
-        let temperature = lerpColor(tempColorfrom, tempColoeto, tempValueMap);
-
-
-        // wind
-        let windValueMap = map(wind, 0, 80, 0, 1);
-        let windValue = wind;
-        let windColorfrom = color('#F397D6');
-        let windColoeto = color('#231942');
-        let windy = lerpColor(windColorfrom, windColoeto, windValueMap);
-
-        // noStroke();
-        //
-        // let strokeSun = sunValueMap * 100;
-        // let strokeRain = rainValue * 20;
-        // let strokeTemperature = tempValue * 2;
-        // let strokeWind = windValue / 10;
-        // console.log(windValue);
-
-        // //sun
-        // noFill();
-        // stroke(sun);
-        // strokeWeight(strokeSun);
-        // ellipse(rectangleXStart += 300, 350, 100, 100);
-        //
-        //
-        // // rain
-        // stroke(rain);
-        // strokeWeight(strokeRain);
-        // ellipse(rectangleXStart, 350, 100, 100);
-        // // line(rectangleXRain += 300, 350, rectangleXRain, 100);
-        // // line(30, 20, 85, 75);
-        // // rect(rectangleXRain += 300, 350, 100, 100);
-        //
-        // stroke(temperature);
-        // strokeWeight(strokeTemperature);
-        // ellipse(rectangleXStart, 350, 100, 100);
-        // // rect(rectangleXTemp += 300, 450, 100, 100);
-        // // line(rectangleXTemp += 300, 350, rectangleXTemp, rectangleXTemp - 100);
-        //
-        // stroke(windy);
-        // strokeWeight(strokeWind);
-        // ellipse(rectangleXStart, 350, 100, 100);
-        //
-        // noStroke();
-        // fill(0);
-        // textSize(20);
-        // text(cityName.location.name, textCoordinates += 300, 600);
-
-
-        circleMaker();
-
-    }
-
-    function circleMaker() {
-
-        console.log('circleMaker');
-        // Try to get to 500
-        while (circles.length < 500) {
-            // Pick a random circle
-            var circle = {
-                // x: random(rectangleXStart += 300, rectangleXEnd += 500),
-                x: random(300, 500),
-                y: random(height - 100),
-                r: random(6, 10)
-            };
-
-            // Does it overlap any previous circles?
-            var overlapping = false;
-            for (var j = 0; j < circles.length; j++) {
-                var other = circles[j];
-                var d = dist(circle.x, circle.y, other.x, other.y);
-                if (d < circle.r + other.r) {
-                    overlapping = true;
-                }
-            }
-
-            // If not keep it!
-            if (!overlapping) {
-                circles.push(circle);
-            }
-
-            // Are we stuck?
-            protection++;
-            if (protection > 10000) {
-                break;
-            }
-        }
-
-        // Draw all the circles
-        for (var i = 0; i < circles.length; i++) {
-            fill(sun);
-            console.log('start', rectangleXStart);
-            console.log('end', rectangleXEnd);
-            noStroke();
-            ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
-        }
-    }
-
 }
